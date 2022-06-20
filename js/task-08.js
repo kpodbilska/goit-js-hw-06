a9297c0df875877c43198730ca6ec2d94e5337b3
kpodbilska <kpodbilska@gmail.com>
2022-06-20 20:51:38 +0200

    next correction

--- js/task-08.js
+++ js/task-08.js
@@ -9,7 +9,7 @@ function handleSubmit(event) {
   } = event.currentTarget;
 
   if (email.value === "" || password.value === "") {
-    return console.log("Please fill in all the fields!");
+    return alert("Please fill in all the fields!");
   }
 
   console.log(`Email: ${email.value}, Password: ${password.value}`);
